226a8e71d6d4088c805f21d8ba0d2bce39bfd954
jherson <rodriguezpaul98@gmail.com>
2025-10-02 16:54:55 -0400

    first commit

---
+++ src/calendar/dto/update-calendar.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCalendarDto } from './create-calendar.dto';
+
+export class UpdateCalendarDto extends PartialType(CreateCalendarDto) {
+  id: number;
+}
